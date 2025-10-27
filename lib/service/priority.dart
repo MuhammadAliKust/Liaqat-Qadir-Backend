cfdea4c426cf90fd37923ed42f80b85bb1218728
Muhammad Ali <kustalics@gmail.com>
2025-10-27 16:31:55 +0500

    Priority Services and Model Done

---
+++ lib/service/priority.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:liaqat_qadir_backend/models/priority.dart';
+
+class PriorityServices {
+  ///Create Priority
+  Future createPriority(PriorityModel model) async {
+    DocumentReference docRef = FirebaseFirestore.instance
+        .collection('priorityCollection')
+        .doc();
+    return await FirebaseFirestore.instance
+        .collection('priorityCollection')
+        .doc(docRef.id)
+        .set(model.toJson(docRef.id));
+  }
+
+  ///Update Priority
+  Future updatePriority(PriorityModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('priorityCollection')
+        .doc(model.docId)
+        .update({'name': model.name});
+  }
+
+  ///Delete Priority
+  Future deletePriority(PriorityModel model) async {
+    return await FirebaseFirestore.instance
+        .collection('priorityCollection')
+        .doc(model.docId)
+        .delete();
+  }
+
+  ///Get All Priority
+  Stream<List<PriorityModel>> getAllPriorities() {
+    return FirebaseFirestore.instance
+        .collection('priorityCollection')
+        .snapshots()
+        .map(
+          (priorityList) => priorityList.docs
+              .map(
+                (priorityJson) => PriorityModel.fromJson(priorityJson.data()),
+              )
+              .toList(),
+        );
+  }
+}
